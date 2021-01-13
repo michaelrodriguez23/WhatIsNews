aecf5399e07fe3c629a664297a4413933fc74db7
Michael Rodriguez <michaelrodriguez23@gmail.com>
2021-01-12 23:05:17 -0500

    added zuck'

--- src/components/Nytimes.jsx
+++ src/components/Nytimes.jsx
@@ -1,32 +1,30 @@
-import React from 'react'
-import p5 from 'p5'
+import React from "react";
+import p5 from "p5";
 // make a flow chart
 class Sketch extends React.Component {
-
   state = {
     count: 1
-
   };
   constructor(props) {
-    super(props)
+    super(props);
     this.myRef = React.createRef();
-
   }
   Sketch = (p) => {
-    let title = 'Transmission of information Shared Via the New York Times';
-    const emailButton = p.createButton('Email');
-    const fbButton = p.createButton('Facebook');
-    const question = p.createElement('h1', title);
-    let headline = p.createElement('p', '');
+
+    let title = "Information Shared Via the New York Times";
+    const emailButton = p.createButton("Email");
+    const fbButton = p.createButton("Facebook");
+    const question = p.createElement("h1", title);
+    let headline = p.createElement("p", "");
     let x = 1;
     let emailedPeriod = 1; // 7, 30 most emailed
-    let apiKey = 'api-key=QAxdBlc0xuqLooRSPDBfuLaec4GwdRhU'
-    let mostEmailed = 'https://api.nytimes.com/svc/mostpopular/v2/emailed/1.json?' + apiKey
-    let mostFacebooked = 'https://api.nytimes.com/svc/mostpopular/v2/shared/1/facebook.json?' + apiKey
+    let apiKey = "api-key=QAxdBlc0xuqLooRSPDBfuLaec4GwdRhU";
+    let mostEmailed = "https://api.nytimes.com/svc/mostpopular/v2/emailed/1.json?" + apiKey;
+    let mostFacebooked = "https://api.nytimes.com/svc/mostpopular/v2/shared/1/facebook.json?" + apiKey;
     let queue = [];
     let keywords = [];
-    let count = this.count
-    let s = '';
+    let count = this.count;
+    let s = "";
     let strings;
     let lead;
     let input,
@@ -37,73 +35,90 @@ class Sketch extends React.Component {
       results,
       y,
       line;
+    let img;
+    p.setup = () => {
+      p.createCanvas(p.windowWidth, p.windowHeight / 1.5);
 
+    };
     p.preload = () => {
       // Preloading the section with question & buttons
-      question.style('text-align:center')
+      img = p.loadImage("mark.jpg");
+      question.style("text-align:center");
       question.center();
+      fbButton.style("background-color:'white';", "color:'green'");
+      emailButton.style("background-color:grey;", "color:white");
       question.position(p.windowWidth / 2, p.windowHeight / 2);
-      emailButton.position(p.windowWidth / 2, p.windowHeight / 4);
-      fbButton.position(p.windowWidth / 2 - 100, p.windowHeight / 4);
-      fbButton.style('background-color:grey;', 'color:white')
-      emailButton.style('background-color:grey;', 'color:white')
-      emailButton.mousePressed(loadEmailResults)
+      emailButton.position(p.windowWidth / 2 - 200, p.windowHeight / 4);
+      fbButton.position(p.windowWidth / 2 - 130, p.windowHeight / 6 + 40);
+
+      emailButton.mousePressed(loadEmailResults);
       fbButton.mousePressed(loadFbResults);
 
-    }
-    p.draw = () => {
-      // drawCircles();
-    }
+    };
 
-    p.setup = () => {
-      p.createCanvas(p.windowWidth, p.windowHeight / 2);
-    }
+    p.draw = () => {
+      p.image(img, 600, 15, 150, 150);
+      // drawCircles();
+    };
 
     function gotData(data) {
       // retrieving data from the NYTIMES API from both most emailed + most shared on fb
       results = data.results;
       getHeadlines();
-      printLead();
+      // printLead();
       printHeadlines();
       getKeywords();
-
     }
 
     // Functions
     // --------------------------------------------------
     function loadFbResults() {
-          x=0;
-       y = p.windowWidth/2;
-      title = 'The most shared articles on Facebook are. ';
+      x = 0;
+      y = p.windowWidth / 2;
+      title = "The most shared articles on Facebook are. ";
       p.loadJSON(mostFacebooked, gotData);
     }
     function loadEmailResults() {
-       y = p.windowWidth/6;
-      title = 'The most emailed articles. ';
+      x = p.windowHeight / 5;
+      y = p.windowWidth / 8;
+      title = "The most emailed articles. ";
       p.loadJSON(mostEmailed, gotData);
     }
     function getHeadlines() {
       //adding the headlines to the end of the queue [] from the NYTIMES api
+
       for (let i = 0; i < 10; i++) {
         queue[i] = results[i].title;
       }
     }
 
     function printLead() {
-      lead = p.createElement('h2', title);
-      p.fill(255)
-      lead.style('text-align:center;')
-      lead.style('padding:0px;');
+      p.fill(255);
+      lead.style("text-align:right;");
+      lead.style("padding:10px;");
     }
 
     function printHeadlines() {
+      // lead = p.createElement("h4", title);
+      console.log(x)
+      console.log(y)
+      console.log(p.windowWidth)
+
       p.fill(255);
+      p.textSize(30);
+      p.text('title', x, y )
+
       for (let i = 0; i < 10; i++) {
         p.textSize(16);
         x = x + 30;
-        line = p.text([i + 1] + '. ' + queue[i], y, x);
+        line = p.text([i + 1] + ". " + queue[i], y, x);
+        if (i % 2 === 0 || i === 0) {
+          p.fill(245, 100, 104);
+        } else {
+          p.fill(255);
+        }
       }
-    };
+    }
 
     function getKeywords() {
       // Iterating through each article, and inside the nested array.
@@ -122,9 +137,9 @@ class Sketch extends React.Component {
       p.stroke(127, 19, 120);
       p.fill(39, 24, 79);
       p.stroke(127, 63, 120);
-      p.circle(p.width / 2 - (x), p.height / 2, x); // left
-      p.circle(p.width / 2, p.height / 2, x - (.8 * x)); // middle
-      p.circle(p.width / 2 + (x), p.height / 2, x + 90); // right
+      p.circle(p.width / 2 - x, p.height / 2, x); // left
+      p.circle(p.width / 2, p.height / 2, x - 0.8 * x); // middle
+      p.circle(p.width / 2 + x, p.height / 2, x + 90); // right
     }
     /*
     const checkVal = () => {
@@ -137,19 +152,17 @@ class Sketch extends React.Component {
     }
   }
   */
-  }
+  };
   componentDidMount() {
-    this.myP5 = new p5(this.Sketch, this.myRef.current)
-
+    this.myP5 = new p5(this.Sketch, this.myRef.current);
   }
 
   render() {
     return (<div ref={this.myRef}>
-      <h1 className="App-title">
-        {this.props.title}</h1>
+      <h1 className="App-title">{this.props.title}</h1>
       <div>{JSON.stringify(this.props.myObj)}</div>
-    </div>)
+    </div>);
   }
 }
 
-export default Sketch
+export default Sketch;
